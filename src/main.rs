88d113b313fc8d8ba354ac2a035016378dae2697
rik0ch3t <rchett02@gmail.com>
2019-11-19 19:30:38 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+  
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::json::{Json, JsonValue};
+
+#[derive(Serialize, Deserialize)]
+pub struct Panthera {
+  pub id: Option<i32>,
+  pub common_name: String,
+  pub scientific_name: String,
+  pub species: String,
+  pub sub_species: String,
+  pub roar: bool
+}
+
+#[post("/", data = "<panthera>")]
+fn create(panthera: Json<Panthera>) -> Json<Panthera> {
+    panthera
+}
+
+#[get("/")]
+fn read() -> Json<JsonValue> {
+    Json(json!([
+        "cat 1",
+        "cat 2"
+    ]))
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/panthera", routes![create])
+        .mount("/pantheras", routes![read])
+        .launch();
+}
